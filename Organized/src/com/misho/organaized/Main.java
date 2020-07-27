b3a4b3f93007ef897b2172c63ddc3459b9353eee
Zgzwang <mikheil_khasaia@yahoo.com>
2020-07-27 15:25:31 +0400

    pluralsight JAVA

---
+++ Organized/src/com/misho/organaized/Main.java
@@ -0,0 +1,14 @@
+package com.misho.organaized;
+
+public class Main {
+   int iVal =5;
+   do {
+
+       System.out.print(iVal);
+       System.out.print("*2 =");
+       iVal *= 2;
+       System.out.println(iVal);
+
+
+    } while(iVal<25);
+}
